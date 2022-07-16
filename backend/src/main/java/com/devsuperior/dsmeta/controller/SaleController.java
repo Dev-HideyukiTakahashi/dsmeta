952228f3e32bd7575b1a96ebe8bd4e8ea4c62a39
Hideyuki Takahashi <yuki.murasaki90@gmail.com>
2022-07-16 17:06:55 -0300

    COMMIT: Date select

--- backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controller/SaleController.java
@@ -1,11 +1,11 @@
 package com.devsuperior.dsmeta.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.model.entities.Sale;
@@ -19,9 +19,11 @@ public class SaleController {
 	private SaleService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Sale>> findSales(){
-		List<Sale> list = service.findSales();
-		return ResponseEntity.ok(list);
+	public Page<Sale> findSales(
+			@RequestParam(value="minDate", defaultValue="") String minDate, 
+			@RequestParam(value="maxDate", defaultValue="") String maxDate, 
+			Pageable pageable){
+		return service.findSales(minDate, maxDate, pageable);
 	}
 	
 }
