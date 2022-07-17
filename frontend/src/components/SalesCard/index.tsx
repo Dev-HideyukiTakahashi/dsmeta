de1ee99bb2d0a9ee865927e1e882a10b25590ede
Hideyuki Takahashi <yuki.murasaki90@gmail.com>
2022-07-17 02:25:09 -0300

    COMMIT: Axios, useEffect first request

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import icon from "../../assets/img/notification-icon.svg";
@@ -7,6 +8,12 @@ function SalesCard() {
   const min = new Date(new Date().setDate(new Date().getDate() - 365));
   const [minDate, setMinDate] = useState(min);
   const [maxDate, setMaxDate] = useState(new Date());
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/sales").then((response) => {
+      console.log(response.data);
+    });
+  }, []);
 
   return (
     <>
